9a8e6d9eb02027f6ac37e6392a5f6f2d63d90206
Mohamadreza Khashei <mohammadrezakhashei1384@gmail.com>
2026-02-11 18:53:12 +0330

    built a simple login page UI and related classes.

---
+++ client/headers/login_window.h
@@ -0,0 +1,55 @@
+//
+// Created by mamzi on 2/11/26.
+//
+
+#ifndef KALANET_LOGIN_WINDOW_H
+#define KALANET_LOGIN_WINDOW_H
+
+#include <QWidget>
+#include <QMainWindow>
+#include <QString>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+    class login_window;
+}
+QT_END_NAMESPACE
+
+class login_window : public QMainWindow {
+    Q_OBJECT
+
+public:
+    explicit login_window(QMainWindow *parent = nullptr);
+    ~login_window() override;
+
+    signals:
+        // Emitted when login is successful (later we'll pass user/session info too)
+        void loginSucceeded(const QString& username);
+
+    // Optional: if you want to open signup page/dialog later
+    void signupRequested();
+
+private slots:
+    void on_btnLogin_clicked();
+    void on_btnRefresh_clicked();
+    void on_btnSignup_clicked();
+
+private:
+    // Captcha
+    void regenerateCaptcha();
+    bool verifyCaptcha(const QString& input) const;
+
+    // Password hashing helper (client-side)
+    QString hashPasswordSha256(const QString& password) const;
+
+    // UI helpers
+    void setError(const QString& message);
+    void clearError();
+
+private:
+    Ui::login_window *ui;
+
+    QString currentCaptcha;
+};
+
+#endif // KALANET_LOGIN_WINDOW_H
