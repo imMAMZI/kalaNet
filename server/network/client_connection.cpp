9c7fb32b43963b8ce5b92fd6b87f733f70ff59c9
Hossein Akbari <hossein8489@gmail.com>
2026-02-14 22:16:27 +0330

    fixed the include

--- server/network/client_connection.cpp
+++ server/network/client_connection.cpp
@@ -1,5 +1,5 @@
 #include "client_connection.h"
-#include "protocol/request_dispatcher.h"
+#include "../protocol/request_dispatcher.h"
 #include "protocol/message.h"
 #include <QJsonDocument>
 #include <QJsonParseError>
